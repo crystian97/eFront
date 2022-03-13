f306d13328af1395f254cd50908a9df4d507ac78
crystian97 <icrystianveloso@gmail.com>
2022-03-13 10:37:36 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-"# eFront" 
+# eFront
+
+![efront.gif](src/img/efront.gif)
+
+Welcome to your best newsletter
+
+## ❗ About
+
+- Introduction
+- Techs
+- Usage
+
+## 📌Introduction
+
+The newsletter of Codelândia , exercise of ebook eFront from Iuri Silva.
+
+## 💻 Techs
+
+- [Javascript](https://www.javascript.com/)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [Sweet Alert](https://sweetalert2.github.io/)
+
+## 📎Usage
+
+```json
+Clone this repository
+
+git clone https://github.com/crystian97/eFront.git
+```
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,55 @@
 btn = document.querySelector(".btn-send");
-btn.addEventListener('click',()=>{
-    console.log('clicou')
-})
+
+function sair(){
+  Swal.close();
+  document.location.reload();
+  }
+btn.addEventListener("click", () => {
+  console.log("clicou");
+  let email = document.getElementById("email").value;
+  let nome = document.getElementById("nome").value;
+  if(email.length>0 && nome.length>0){
+    Swal.fire({
+      
+      html:
+        
+        'E-mail cadastrado com  '+
+        '<span class="text-success">sucesso </span> ' +
+        '<br>'+
+        '<button class="btn-close-success btn-close" onclick="sair()">Fechar</button>',
+
+      showCloseButton: false,
+      showCancelButton: false,
+      showConfirmButton:false,
+      focusConfirm: false,
+      theme:'dark',
+      
+      showCloseButton:false,
+      showTitle:false
+      
+  })
+  }else{
+    Swal.fire({
+      
+        html:
+          
+          'E-mail  '+
+          '<span class="text-error">não </span> ' +
+          'cadastrado!'+
+          '<br>'+
+          '<button class="btn-close-error btn-close" onclick="sair()">Fechar</button>',
+
+        showCloseButton: false,
+        showCancelButton: false,
+        showConfirmButton:false,
+        focusConfirm: false,
+        theme:'dark',
+        
+        showCloseButton:false,
+        showTitle:false
+        
+    })
+  }
+});
+
+
